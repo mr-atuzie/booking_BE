8e5a347d307653d578b16f96ceca0bcb7be0fb74
mr-atuzie <user email>
2024-05-04 14:43:34 +0100

    cors

--- index.js
+++ index.js
@@ -9,9 +9,6 @@ const connectDB = require("./config/db");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const userRoute = require("./routes/user");
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(
   cors({
     origin: [
@@ -21,6 +18,10 @@ app.use(
     credentials: true,
   })
 );
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use("/api/v1/user", userRoute);
 
